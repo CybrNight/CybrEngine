006067a22383e0c7e15d9c2d37104a8b71e70f5a
Nathan Estrada <naestrada19@gmail.com>
2024-04-07 18:34:56 -0700

    Basic Entity and Handler logic

---
+++ Entity.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CybrEngine {
+    public abstract class Entity {
+
+        //Private references to engine systems
+
+        private Vector2 _positiion;
+        private Texture2D _sprite;
+        private Handler handler;
+        private ComponentList cList;
+
+        public Vector2 position{ 
+            get{ return _positiion; }
+            set{ _positiion = value; }
+        }
+
+        public Texture2D sprite {
+            get { return _sprite; }
+        }
+
+        protected Entity(){
+            handler = Handler.Instance;
+        }
+
+        public virtual void SetSprite(string name){
+            _sprite = Assets.GetSprite(name);
+        }
+
+
+        public virtual void Update(){
+
+        }
+
+
+        protected bool Destroyed = false;
+        protected bool BeingDestroyed = false;
+        
+        public bool IsDestroyed {
+            get {
+                return Destroyed || BeingDestroyed;
+            }
+        }
+
+        protected void Destory(Entity entity){
+            entity.Destroyed = true;
+        }
+
+        protected void Instantiate(Entity entity){
+            handler.Instantiate(entity);
+        }
+
+        protected void AddComponenent(Type cType){
+            cList.AddComponent(this, cType);
+        }
+
+        protected void GetComponent<T>(Entity entity){
+            cList.GetComponent<T>(entity);
+        }
+    }
+}
---
+++ Handler.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CybrEngine {
+    internal class Handler {
+
+        private static List<Entity> entities = new List<Entity>();
+
+        private static Handler _instance;
+        public static Handler Instance {
+            get {
+                if(_instance == null){
+                    _instance = new Handler();
+                }
+                return _instance;
+            }
+        }
+
+        private Handler(){
+
+        }
+
+        public void Instantiate(Entity entity){
+            entities.Add(entity);
+        }
+
+        public void Update(){
+            for(int i = 0; i < entities.Count; i++){
+                Entity e = entities[i];
+                Console.WriteLine(e.position.ToString());
+                if(e.IsDestroyed){
+                    entities.Remove(e);
+                }
+
+                e.Update();
+            }
+        }
+    
+        public void Draw(SpriteBatch spriteBatch){
+            for (int i = 0; i < entities.Count;i++){
+                Entity e = entities[i];
+                Texture2D sprite = e.sprite;
+
+                spriteBatch.Begin();
+                spriteBatch.Draw(sprite, e.position, null, Color.White, 0f,
+                new Vector2(sprite.Width / 2, sprite.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f);
+                spriteBatch.End();
+            }
+    }
+        }    
+        }
