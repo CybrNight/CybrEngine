b1426ba4c97eeb3f44905a7797e7cd619cd98abe
Nathan Estrada <naestrada19@gmail.com>
2024-04-12 04:18:54 -0700

    Handler class rewrite start

--- Handler.cs
+++ Handler.cs
@@ -12,56 +12,68 @@ namespace CybrEngine {
     internal class Handler {
 
         private static List<Entity> entities = new List<Entity>();
+        private static List<Transform> transforms = new List<Transform>();
+        private static List<IComponentUpdate>
 
         private static Handler _instance;
         public static Handler Instance {
             get {
-                if(_instance == null){
+                if(_instance == null) {
                     _instance = new Handler();
                 }
                 return _instance;
             }
         }
 
-        private Handler(){
+        private Handler() {
 
         }
 
-        public void Instantiate(Entity entity){
+        public void Instantiate(Entity entity) {
             entities.Add(entity);
+
+            var transform = entity.GetComponent<Transform>();
+            if(transform) {
+                transforms.Add(transform);
+            }
         }
 
-        public void Update(){
-            for(int i = 0; i < entities.Count; i++){
+        public void Update() {
+            for(int i = 0; i < entities.Count; i++) {
                 Entity e = entities[i];
-                if(e.IsDestroyed){
+                if(e.IsDestroyed) {
                     entities.Remove(e);
                 }
-                e.Update();
+            }
 
-                var t1 = e.GetComponent<Transform>();
-                if(t1) {
-                    Debug.WriteLine("Hello1");
-                    for(int j = i + 1; j < entities.Count; j++) {
-                        Entity o = entities[j];
-                        var t2 = o.GetComponent<Transform>();
-                        if (t2) {
-                            if (t1.Intersects(t2)){
-                                Debug.WriteLine("t1 touch t2");
-                            }
-                        }
+            for(int i = 0; i < transforms.Count; i++) {
+                var t1 = transforms[i];
+                for(int j = 0; j < transforms.Count; j++) {
+                    var t2 = transforms[j];
+
+                    //Check that t1 and t2 are not same entity
+                    if(t1 == t2)
+                        continue;
+                    if(t1.Intersects(t2)) {
+                        Debug.WriteLine("t1 intersect t2");
                     }
+
                 }
-                
-                
             }
-        }
-    
-        public void Draw(SpriteBatch batch){
-            for (int i = 0; i < entities.Count;i++){
+
+            for(int i = 0; i < entities.Count; i++) {
                 Entity e = entities[i];
-                e.Draw(batch);
+                e.Update();
             }
-    }
-        }    
+
         }
+
+        public void Draw(SpriteBatch batch) {
+
+            for(int i = 0; i < transforms.Count; i++) {
+                var t1 = transforms[i];
+                t1.Draw(batch);
+            }
+        }
+    }
+}
