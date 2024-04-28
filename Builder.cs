fad3277fdceb0f452caecf8ce0a663e5cf61d253
Nathan Estrada <naestrada19@gmail.com>
2024-04-27 23:59:57 -0700

    Created Builder class to create instances of private types

---
+++ Builder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CybrEngine {
+    internal static class Builder {
+        public static T Component<T>(int entity) where T : Component {
+            T newComponent = (T)Activator.CreateInstance(typeof(T), true);
+            return newComponent;
+        }
+
+        public static T Entity<T>() where T : Entity {
+            T newComponent = (T)Activator.CreateInstance(typeof(T), true);
+            return newComponent;
+        }
+
+        public static T Object<T>(object[] deps = null) where T : Object {
+            T newComponent = (T)Activator.CreateInstance(typeof(T), true, deps);
+            return newComponent;
+        }
+    }
+}
