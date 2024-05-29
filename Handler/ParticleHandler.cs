5f4a96907cddd7eafff8f2d856fcc097b70b5115
Nathan Estrada <naestrada19@gmail.com>
2024-05-28 20:34:11 -0700

    Basic particle emitter added

---
+++ Handler/ParticleHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CybrEngine {
+    public class ParticleHandler {
+
+        private static ParticleHandler instance;
+        public static ParticleHandler Instance {
+            get {
+                if (instance == null) {
+                    instance = new ParticleHandler();
+                }
+                return instance;
+            }
+        }
+
+        private ParticleHandler(){
+            
+        }
+
+        private List<Particle> particles = new List<Particle>();
+
+        public void Draw(SpriteBatch spriteBatch) {
+            for(int i = 0; i < particles.Count; i++) {
+                var particle = particles[i];
+                if(!particle.IsDestroyed) {
+                    particle.Draw(spriteBatch);
+                } else {
+                    particles.Remove(particle);
+                }
+            }
+        }
+
+        public Particle Emit(Particle particle, Vector2 position) {
+            particle.Transform.Position = position;
+            particles.Add(particle);
+            return particle;
+        }
+    }
+}
---
+++ Particle/Particle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CybrEngine {
+    public abstract class Particle : Object{
+        
+        public Transform Transform { get; set; } = new Transform();
+        public Rectangle Bounds { get; set; }
+        public Color Color { get; set; }
+        public float Life { get; set; }
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
